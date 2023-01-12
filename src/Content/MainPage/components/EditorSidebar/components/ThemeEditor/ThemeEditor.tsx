1a8051ad6f07add5ebae48531c4b36c296a75642
nik <playpaper12@gmail.com>
2023-01-12 09:40:25 +0200

    added ids for themes

--- src/Content/MainPage/components/EditorSidebar/components/ThemeEditor/ThemeEditor.tsx
+++ src/Content/MainPage/components/EditorSidebar/components/ThemeEditor/ThemeEditor.tsx
@@ -6,34 +6,40 @@ const themes = [
 	{
 		backgroundColor: getCssVariable('--ft-color-blue'),
 		textColor: getCssVariable('--ft-color-white'),
+		id: '1',
 	},
 	{
 		textColor: getCssVariable('--ft-color-blue'),
+		id: '2',
 	},
 	{
 		backgroundColor: getCssVariable('--ft-color-yellow'),
 		textColor: getCssVariable('--ft-color-white'),
+		id: '3',
 	},
 	{
 		backgroundColor: getCssVariable('--ft-color-orange'),
 		textColor: getCssVariable('--ft-color-white'),
+		id: '4',
 	},
 	{
 		backgroundColor: getCssVariable('--ft-color-green'),
 		textColor: getCssVariable('--ft-color-white'),
+		id: '5',
 	},
 	{
 		backgroundColor: getCssVariable('--ft-color-pink'),
 		textColor: getCssVariable('--ft-color-white'),
+		id: '6',
 	},
 ]
 
 export const ThemeEditor = () => {
 	return (
 		<div className={styles.container}>
-			{themes.map(({ backgroundColor, textColor }) => (
+			{themes.map(({ backgroundColor, textColor, id }) => (
 				<div
-					key={`${backgroundColor}-${textColor}`}
+					key={id}
 					className={styles.themeItem}
 					style={{
 						backgroundColor,
