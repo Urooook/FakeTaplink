d3ebf7ac08a3fed2a4a54bea682ef824c6b5e36a
nik <playpaper12@gmail.com>
2023-01-14 08:42:00 +0200

    added remove feature for all editors

--- src/Pages/MainPage/components/ControlsSidebar/enum.ts
+++ src/Pages/MainPage/components/ControlsSidebar/enum.ts
@@ -3,8 +3,8 @@ import { CompoentNames } from '../../blocksContext'
 export const ControlIds = {
 	themes: 'themes',
 	text: CompoentNames.TextBlock,
-	html: 'html',
+	html: CompoentNames.HtmlLink,
 	link: CompoentNames.LinkBlock,
-	image: 'image',
+	image: CompoentNames.ImageLink,
 	social: 'social',
 }
--- src/Pages/MainPage/components/EditorSidebar/components/HtmlEditor/HtmlEditor.tsx
+++ src/Pages/MainPage/components/EditorSidebar/components/HtmlEditor/HtmlEditor.tsx
@@ -1,211 +1,196 @@
-import React, {useContext, useEffect, useState} from 'react';
-import {Textarea} from "../../../../../../components/UI/Textarea/Textarea";
-import styles from "./HtmlEditor.module.css";
-import {Button} from "../../../../../../components/UI/Button/Button";
-import {opt, or, repeat, seq, tag, take, ws} from "../../../../../../helpers/Parser/Factories";
-import {binsearchKey} from "../../../../../../helpers/Parser/htmlTags";
-import {Parser} from "../../../../../../helpers/Parser/types";
-import {TextBlock} from "../../../blocks/TextBlock/TextBlock";
-import {BlocksContext, CompoentNames, ComponentBlock, TextBlockProps} from "../../../../blocksContext";
-import {HtmlBlock} from "../../../blocks/HtmlBlock/HtmlBlock";
+import React, { useContext, useEffect, useState } from 'react'
+import { Textarea } from '../../../../../../components/UI/Textarea/Textarea'
+import styles from './HtmlEditor.module.css'
+import { Button } from '../../../../../../components/UI/Button/Button'
+import { opt, or, repeat, seq, tag, take, ws } from '../../../../../../helpers/Parser/Factories'
+import { binsearchKey } from '../../../../../../helpers/Parser/htmlTags'
+import { Parser } from '../../../../../../helpers/Parser/types'
+import { TextBlock } from '../../../blocks/TextBlock/TextBlock'
+import {
+	BlocksContext,
+	CompoentNames,
+	ComponentBlock,
+	TextBlockProps,
+} from '../../../../blocksContext'
+import { HtmlBlock } from '../../../blocks/HtmlBlock/HtmlBlock'
 
-type parserResult = {
-    res: string,
-    errors: string[]
+type ParserResult = {
+	res: string
+	errors: string[]
 }
 
 const initialTextEditorState: TextBlockProps = {
-    text: '',
+	text: '',
 }
 
 export const HtmlEditor = () => {
-    const [errors, setErrors] = useState<string[]>([]);
-    const [html, setHtml] = useState<string>('');
-    const [compile, setCompile] = useState('');
-    const { blocks, activeBlock, onAddBlock, onDeleteBlock } = useContext(BlocksContext);
+	const [errors, setErrors] = useState<string[]>([])
+	const [html, setHtml] = useState<string>('')
+	const [compile, setCompile] = useState('')
+	const { blocks, activeBlock, onAddBlock, onDeleteBlock } = useContext(BlocksContext)
 
-    useEffect(() => {
-        if (!activeBlock && onAddBlock) {
-            onAddBlock({
-                value: { ...initialTextEditorState },
-                component: <TextBlock text={''} />,
-                componentName: CompoentNames.HtmlLink,
-            })
-        }
-    }, [activeBlock])
+	useEffect(() => {
+		if (!activeBlock && onAddBlock) {
+			onAddBlock({
+				value: { ...initialTextEditorState },
+				component: <TextBlock text={''} />,
+				componentName: CompoentNames.HtmlLink,
+			})
+		}
+	}, [activeBlock])
 
-    const xmlNameRgxp = /[^ <>"'=/]/;
+	const xmlNameRgxp = /[^ <>"'=/]/
 
-    const xmlAttrValue = seq(
-        tag('='),
-        ws,
-        tag('"'),
-        take(/[^"]/, {token: "CREATE_ATTR_VALUE"}),
-        tag('"')
-    )
+	const xmlAttrValue = seq(
+		tag('='),
+		ws,
+		tag('"'),
+		take(/[^"]/, { token: 'CREATE_ATTR_VALUE' }),
+		tag('"'),
+	)
 
-    const xmlAttr = seq(
-        ws,
-        take(xmlNameRgxp, {token: "CREATE_ATTE_KEY"}),
-        ws,
-        opt(xmlAttrValue)
-    )
+	const xmlAttr = seq(ws, take(xmlNameRgxp, { token: 'CREATE_ATTE_KEY' }), ws, opt(xmlAttrValue))
 
-    const xmlTag = seq(
-        ws,
-        tag('<'),
-        ws,
-        take(xmlNameRgxp, {token: 'CREATE_TAG'}),
-        ws,
-        repeat(xmlAttr, {min: 0}),
-        ws,
-        tag('>', {token: 'END_CREATE_TAG'})
-    )
+	const xmlTag = seq(
+		ws,
+		tag('<'),
+		ws,
+		take(xmlNameRgxp, { token: 'CREATE_TAG' }),
+		ws,
+		repeat(xmlAttr, { min: 0 }),
+		ws,
+		tag('>', { token: 'END_CREATE_TAG' }),
+	)
 
-    const xmlEndTag = seq(
-        ws,
-        tag('</'),
-        ws,
-        take(xmlNameRgxp, {token: 'END_TAG'}),
-        ws,
-        tag('>' )
-    );
+	const xmlEndTag = seq(ws, tag('</'), ws, take(xmlNameRgxp, { token: 'END_TAG' }), ws, tag('>'))
 
-    const xmlText = seq(
-        take(/[^<>]/, {token: 'TEXT'})
-    );
+	const xmlText = seq(take(/[^<>]/, { token: 'TEXT' }))
 
-    const xml: Parser = repeat(
-        or(
-            seq(
-                xmlTag,
-                opt((...args) => or(
-                    seq(xmlText, xml, opt(xmlText)),
-                    xml
-                )(...args)),
-                xmlEndTag
-            ),
-            xmlText
-        ),
-        {min: 0}
-    )
+	const xml: Parser = repeat(
+		or(
+			seq(
+				xmlTag,
+				opt((...args) => or(seq(xmlText, xml, opt(xmlText)), xml)(...args)),
+				xmlEndTag,
+			),
+			xmlText,
+		),
+		{ min: 0 },
+	)
 
-    const parseHtml = (htmlArr: any) => {
-        const tokens = htmlArr
-        const stackStartTags = [];
-        const stackEndTags = [];
-        const errorMessages = [];
-        let depth = 0;
-        let res = '';
+	const parseHtml = (htmlArr: any) => {
+		const tokens = htmlArr
+		const stackStartTags = []
+		const stackEndTags = []
+		const errorMessages = []
+		let depth = 0
+		let res = ''
 
-        // @ts-ignore
-        for(const token of tokens) {
-            const val = token.value;
+		// @ts-ignore
+		for (const token of tokens) {
+			const val = token.value
 
-            switch (token.type){
-                case 'CREATE_TAG': {
-                    const isHtmlTag = binsearchKey(val)
-                    if(isHtmlTag === -1) {
-                        errorMessages.push(`${val} не является html тегом`);
-                    }
-                    stackStartTags.push(val);
-                    res+= ' '.repeat(depth)
-                    res += `<span style="color: red;">&lt${val}</span>`;
-                    depth+= 3;
-                    break;
-                }
-                case 'CREATE_ATTE_KEY': {
-                    res += ` <span style="color: #101b98;">${val}</span>`;
-                    break;
-                }
-                case 'CREATE_ATTR_VALUE': {
-                    res+= `=<span style="color: green;">"${val}"</span>`;
-                    break;
-                }
-                case 'END_CREATE_TAG': {
-                    res += `<span style="color: red;">&gt</span> \n`
-                    break;
-                }
-                case 'END_TAG': {
-                    stackEndTags.push(val);
-                    depth-= 3;
-                    res+= ' '.repeat(depth)
-                    res+= `<span style="color: red;">&lt/${val}&gt</span>\n`;
-                    break;
-                }
-                case 'TEXT': {
-                    const a = depth+1
-                    res+= ' '.repeat(a)
-                    if( val === '\n') {
-                        res += val;
-                    } else {
-                        res += `<span style="color: black; font-family: Arial">${val}</span> \n`;
-                    }
-                    break;
-                }
-            }
-        }
+			switch (token.type) {
+				case 'CREATE_TAG': {
+					const isHtmlTag = binsearchKey(val)
+					if (isHtmlTag === -1) {
+						errorMessages.push(`${val} не является html тегом`)
+					}
+					stackStartTags.push(val)
+					res += ' '.repeat(depth)
+					res += `<span style="color: red;">&lt${val}</span>`
+					depth += 3
+					break
+				}
+				case 'CREATE_ATTE_KEY': {
+					res += ` <span style="color: #101b98;">${val}</span>`
+					break
+				}
+				case 'CREATE_ATTR_VALUE': {
+					res += `=<span style="color: green;">"${val}"</span>`
+					break
+				}
+				case 'END_CREATE_TAG': {
+					res += `<span style="color: red;">&gt</span> \n`
+					break
+				}
+				case 'END_TAG': {
+					stackEndTags.push(val)
+					depth -= 3
+					res += ' '.repeat(depth)
+					res += `<span style="color: red;">&lt/${val}&gt</span>\n`
+					break
+				}
+				case 'TEXT': {
+					const a = depth + 1
+					res += ' '.repeat(a)
+					if (val === '\n') {
+						res += val
+					} else {
+						res += `<span style="color: black; font-family: Arial">${val}</span> \n`
+					}
+					break
+				}
+			}
+		}
 
-        return {res, errors: errorMessages};
-    }
+		return { res, errors: errorMessages }
+	}
 
-    const minify = (str: string): string => str.replace(/(\s)\1+/gm, '').replace(/\n/gm, '');
+	const minify = (str: string): string => str.replace(/(\s)\1+/gm, '').replace(/\n/gm, '')
 
-    if (!onAddBlock || !onDeleteBlock) {
-        return null
-    }
+	if (!onAddBlock || !onDeleteBlock) {
+		return null
+	}
 
-    const write = (str: string) => {
+	const write = (str: string) => {
+		const p = xml('a s')
+		p.next()
+		p.next(minify(`${str}`))
 
-        const p = xml('a s')
-        p.next()
-        p.next(minify(`${str}`))
+		// @ts-ignore
+		const arr = [...p]
+		const result = parseHtml(arr)
+		setCompile(result.res)
+		setErrors(result.errors)
 
-        // @ts-ignore
-        const arr = [...p]
-        const result = parseHtml(arr);
-        setCompile(result.res);
-        setErrors(result.errors)
+		if (activeBlock?.id) {
+			const currentBlock = blocks.get(activeBlock?.id) as ComponentBlock<TextBlockProps>
 
-        if (activeBlock?.id) {
-            const currentBlock = blocks.get(activeBlock?.id) as ComponentBlock<TextBlockProps>
+			if (currentBlock) {
+				currentBlock.value['text'] = str
+				onAddBlock({
+					...currentBlock,
+					component: <HtmlBlock text={currentBlock.value.text} />,
+				})
+			}
+		}
+	}
 
-            if (currentBlock) {
-                currentBlock.value['text'] = str;
-                onAddBlock({
-                    ...currentBlock,
-                    component: <HtmlBlock text={currentBlock.value.text} />,
-                })
-            }
-        }
-    }
+	const handleDeleteBlock = () => {
+		onDeleteBlock(activeBlock?.id)
+	}
 
+	return (
+		<>
+			{errors.length > 0 && errors.map((err) => <div style={{ color: 'red' }}>{err}</div>)}
 
+			<Textarea
+				value={html}
+				onChange={(e) => {
+					setHtml(e.target.value)
+					write(e.target.value)
+				}}
+				className={styles.htmlCode}
+			/>
 
+			<pre>
+				<code>
+					<div dangerouslySetInnerHTML={{ __html: compile }} />
+				</code>
+			</pre>
 
-
-    return (
-        <>
-            {
-                errors.length > 0 && errors.map((err) => <div style={{color: 'red'}}>{err}</div>)
-            }
-
-            <Textarea
-                value={html}
-                onChange={(e) => {
-                    setHtml(e.target.value);
-                    write(e.target.value);
-                }}
-                className={styles.htmlCode}
-            />
-
-            <pre>
-                <code>
-                     <div dangerouslySetInnerHTML={{ __html: compile }} />
-                </code>
-            </pre>
-
-            <Button>Добавить</Button>
-        </>
-    )
-};
+			<Button onClick={handleDeleteBlock}>Удалить</Button>
+		</>
+	)
+}
